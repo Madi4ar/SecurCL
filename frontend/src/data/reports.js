62d20b1ad0e92b0eb13c67271c6df5c728f35628
Madiyar Talgatuly <133092635+Madi4ar@users.noreply.github.com>
2024-05-20 20:28:49 +0500

    Add files via upload

---
+++ frontend/src/data/reports.js
@@ -0,0 +1,22 @@
+export const Reports = [
+  {
+    date: '12/16 - 17:39:55',
+    title:
+      '(**] [1:10003:1] TCP Connection Attempt Detected [**] [Priority: 0] (TCP} 192.168.152.128:44852 > 192.168.152.130:22',
+  },
+  {
+    date: '12/16-17:39:55',
+    title:
+      '[**] [1:10004:1] Possible SSH Brute Force Attack [**] [Priority: 0] {TCP) 192.168.152. 128:44864 -> 192.168.152.130:22',
+  },
+  {
+    date: '12/15-07:39:55',
+    title:
+      '[**] [1:10003:1] TCP Connection Attempt Detected [**] [Priority: 0] {TCP} 192.168.152.128:44864 -> 192.168. 152. 130:22',
+  },
+  {
+    date: '12/10-19:09:10',
+    title:
+      '[**] [1:10002:1] Possible SSH Brute Force Attack [**] [Priority: 0] {TCP} 192.168.152.128:44864 -> 192.168.152.130:22',
+  },
+];
