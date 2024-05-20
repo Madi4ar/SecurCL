f94af47334123d8175cac81bd0ee20730179c28b
Madiyar Talgatuly <133092635+Madi4ar@users.noreply.github.com>
2024-05-20 20:25:02 +0500

    Add files via upload

--- frontend/src/app/main/events/page.js
+++ frontend/src/app/main/events/page.js
@@ -1,7 +1,22 @@
+import Title from '@/components/Title';
+import { Reports } from '@/data/reports';
+
 export default function Events() {
   return (
     <>
-      <div>Events page</div>
+      <div className="px-5 mt-5 ml-3 mr-3 bg-white rounded-xl">
+        <Title title="Events" />
+        <div className="flex flex-col py-10">
+          {Reports.map((report) => {
+            return (
+              <div className="rounded-xl bg-white shadow-md p-4 mt-3">
+                <p className="text-black">{report.date}</p>
+                <p className="text-black">{report.title}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
--- frontend/src/app/main/files/page.js
+++ frontend/src/app/main/files/page.js
@@ -1,7 +1,14 @@
+import File from '@/components/File';
+
 export default function Files() {
   return (
     <>
-      <div>Files page</div>
+      <div className="p-5 ml-3 mr-3 bg-white rounded-xl mt-5">
+        <h1 className="text-xl font-bold uppercase">Last Uploaded Files</h1>
+        <div className="flex flex-wrap">
+          <File title="Anau mnau.zip" size="14" />
+        </div>
+      </div>
     </>
   );
 }
